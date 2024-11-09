0c7a7c451a8a313d22b75b1125a474a8a23db550
teotof <hadjitot@tcd.ie>
2024-11-08 19:09:02 -0800

    Exercises Done

--- server/src/createTable.ts
+++ server/src/createTable.ts
@@ -2,20 +2,20 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
 const initDB = async () => {
-    // Open the database connection
-    const db = await open({
-        filename: "database.sqlite",
-        driver: sqlite3.Database,
-    });
-    // Create a "budget" table if it doesn't exist
-    await db.exec(`
+  // Open the database connection
+  const db = await open({
+    filename: "database.sqlite",
+    driver: sqlite3.Database,
+  });
+  // Create a "budget" table if it doesn't exist
+  await db.exec(`
    CREATE TABLE IF NOT EXISTS expenses (
      id TEXT PRIMARY KEY,
      description TEXT NOT NULL,
      cost INTEGER NOT NULL
    );
  `);
-    return db;
+  return db;
 };
 
 export default initDB;
